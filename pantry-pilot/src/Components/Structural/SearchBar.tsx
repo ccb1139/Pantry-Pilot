9c1e33fb4bf03379281b447162997998e6280bfa
ccb1139 <calebbergen000@gmail.com>
2023-04-05 14:02:36 -0400

    stuff

--- pantry-pilot/src/Components/Structural/SearchBar.tsx
+++ pantry-pilot/src/Components/Structural/SearchBar.tsx
@@ -1,12 +1,18 @@
-import React, { useState, useEffect, useRef, useMemo } from 'react'
+import React, { useState, useEffect, useRef, useMemo, ChangeEvent } from 'react'
 import debounce from 'lodash.debounce';
 
 
 //Icon Imports
 import { AiOutlineClose } from 'react-icons/ai'
 
-function SearchBar({ search, setSearch }) {
-    const searchRef = useRef(null);
+
+type Props = {
+    search: string,
+    setSearch: React.Dispatch<React.SetStateAction<string>>
+}
+
+function SearchBar({ search, setSearch }: Props) {
+    const searchRef = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
         return () => {
@@ -15,14 +21,14 @@ function SearchBar({ search, setSearch }) {
     });
 
 
-    const handleClear = (event) => {
-        searchRef.current.value = "";
-        setSearch("");
-    }
+    const handleClear = (event: React.MouseEvent<SVGElement, MouseEvent>): void => {
+        searchRef.current!.value = '';
+        setSearch('');
+      };
 
-    const handleSearchChange = (event) => {
-        setSearch(searchRef.current.value);
-    }
+      const handleSearchChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        setSearch(event.target.value);
+      };
 
     const _handleSearchChange = useMemo(() => {
         return debounce(handleSearchChange, 200);
