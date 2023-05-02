64c15b29a08a835cb68342196d5d8d9f6348f153
ccb1139 <calebbergen000@gmail.com>
2023-05-02 14:05:31 -0400

    Timer Changes

--- pantry-pilot/src/Components/Recipes/Page/TimeInput.tsx
+++ pantry-pilot/src/Components/Recipes/Page/TimeInput.tsx
@@ -1,39 +1,89 @@
-import React, { useState, ChangeEvent, FormEvent } from 'react';
+import React, { useState, ChangeEvent, useRef, FormEvent, useEffect } from 'react';
 
 interface Props {
-  onSetTime: (totalSeconds: number) => void;
+  onSetTime: any;
 }
 
 const CountdownInput: React.FC<Props> = ({ onSetTime }) => {
   const [timeString, setTimeString] = useState<string>('');
+  const [timerTime, setTimerTime] = useState<string[]>(['0', '0', '0']);
 
-  const handleTimeChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-    const regex = /^([0-9]|0[0-9]|1[0-9]|2[0-3]);([0-5][0-9]);([0-5][0-9])$/; // Regular expression for valid time input
+  const hourRef = useRef<any>(null);
+  const minRef = useRef<any>(null);
+  const secRef = useRef<any>(null);
 
-    if (regex.test(value) || value === '') { // Only update the state if the value is a valid time input or an empty string
-      setTimeString(value);
+
+
+  const handleTimeChange = (event: any) => {
+    console.log(event.target.className)
+    const { className, value } = event.target;
+    // if (value.length === 1) {
+    //   event.target.value = '0' + value;
+    // } else if (value.length !== 0) {
+    //   event.target.value = parseInt(value);
+    // }
+    switch (className) {
+      case 'timer-hour':
+        if (value > 99) {
+          hourRef.current.value = 99;
+        } else if (value < 0) {
+          hourRef.current.value = 0;
+        }
+        setTimerTime([value, timerTime[1], timerTime[2]]);
+        break;
+      case 'timer-min':
+        if (value > 59) {
+          minRef.current.value = 59;
+        } else if (value < 0) {
+          minRef.current.value = 0;
+        }
+        setTimerTime([timerTime[0], value, timerTime[2]]);
+        break;
+      case 'timer-sec':
+        if (value > 59) {
+          secRef.current.value = 59;
+        } else if (value < 0) {
+          secRef.current.value = 0;
+        }
+        setTimerTime([timerTime[0], timerTime[1], value]);
+        break;
+      default:
+        break;
     }
-    setTimeString(timeString);
+
   };
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  useEffect(() => {
+    console.log(timerTime)
+  }, [timerTime])
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement> | React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     event.preventDefault();
-    const timeParts = timeString.split(';');
-    const hours = parseInt(timeParts[0]) || 0;
-    const minutes = parseInt(timeParts[1]) || 0;
-    const seconds = parseInt(timeParts[2]) || 0;
-    const totalSeconds = hours * 3600 + minutes * 60 + seconds;
-    onSetTime(totalSeconds);
+    // const timeParts = timeString.split(';');
+    // const hours = parseInt(timeParts[0]) || 0;
+    // const minutes = parseInt(timeParts[1]) || 0;
+    // const seconds = parseInt(timeParts[2]) || 0;
+    // const totalSeconds = hours * 3600 + minutes * 60 + seconds;
+    // console.log(timerTime)
+    onSetTime(timerTime);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Countdown Time (HH;MM;SS):
-        <input type="text" defaultValue={timeString} onChange={handleTimeChange} />
-      </label>
-      <button type="submit">Start Countdown</button>
+    <form onSubmit={handleSubmit} className='timer-input-display'>
+
+      <div className='recipe-utility-timer-header '>
+        <input className='timer-hour' ref={hourRef} type='number' max={99} placeholder='hour' onChange={(e) => { handleTimeChange(e) }} />
+        <span> : </span>
+        <input className='timer-min' ref={minRef} type='number' max={59} placeholder='min' onChange={(e) => { handleTimeChange(e) }} />
+        <span> : </span>
+        <input className='timer-sec' ref={secRef} type='number' max={59} placeholder='sec' onChange={(e) => { handleTimeChange(e) }} />
+        {/* <TimeInput onSetTime={setTimerTime} /> */}
+      </div>
+
+      <div className='custom-tab-btn' onClick={(e) => { handleSubmit(e); }}>
+        Start
+      </div>
+      {/* <button type="submit" style={{display: 'none'}}></button> */}
     </form>
   );
 };
--- pantry-pilot/src/Components/Recipes/Page/Timer.tsx
+++ pantry-pilot/src/Components/Recipes/Page/Timer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useLayoutEffect } from 'react'
+import React, { useState, useEffect, useRef, useLayoutEffect, useCallback } from 'react'
 
 // Components
 import TimeInput from './TimeInput'
@@ -6,40 +6,17 @@ import TimeInput from './TimeInput'
 // Timer
 import { useCountdown, CountdownCircleTimer } from 'react-countdown-circle-timer'
 
+// Icon Imports
+import { FaPlay, FaPause, FaStop } from 'react-icons/fa'
+import { AiOutlineClose } from 'react-icons/ai'
+
 type Props = {}
 
 function Timer({ }: Props) {
     const [timerGoing, setTimerGoing] = useState<boolean>(false);
-    const [timerTime, setTimerTime] = useState<number>(2);
+    const [timerTimeSeconds, setTimerTimeSeconds] = useState<number>(0);
+
     const [key, setKey] = useState(0);
-
-    const [times, setTimes] = useState(['', '', '', '', '', '']);
-
-    function handleChange(index: any, event: any) {
-        const newTimes = [...times];
-        newTimes[index] = event.target.value;
-        for (let i = index + 1; i < 6; i++) {
-            newTimes[i] = '';
-            newTimes[i] = times[i - 1];
-            
-        }
-        setTimes(newTimes);
-    }
-
-    const TimerInput = () => {
-        
-        return (
-        <div className='timer-hour'>
-            {times.map((time, index) => (
-                <input
-                    key={index}
-                    type="number"
-                    value={time}
-                    onChange={(event) => handleChange(index, event)}
-                />
-            ))}
-        </div>
-    )}
 
     const {
         path,
@@ -51,44 +28,112 @@ function Timer({ }: Props) {
         size,
         strokeWidth,
     } = useCountdown({ isPlaying: true, duration: 7, colors: '#abc' })
+    console.log(remainingTime, elapsedTime, pathLength)
 
-    function changeTimerTime(time: number) {
-        setTimerTime(time)
-        setKey(prevKey => prevKey + 1)
+    function startTimer(timeArr: string[]) {
+
+        setTimerTimeSeconds(timeToSeconds(timeArr));
+        setKey(prevKey => prevKey + 1);
+        console.log(timerTimeSeconds)
+        setTimerGoing(true)
     }
+    function resetTimer() {
+        setTimerGoing(false);
+        setTimerTimeSeconds(0);
+        setKey(prevKey => prevKey + 1);
+    }
+
+    const formatTime = useCallback((timeArr: string[]): string => {
+        const [hours, minutes, seconds] = timeArr;
+        const hoursStr = hours.toString().padStart(2, '0');
+        const minutesStr = minutes.toString().padStart(2, '0');
+        const secondsStr = seconds.toString().padStart(2, '0');
+        const timeString = `${hoursStr}:${minutesStr}:${secondsStr}`;
+        return timeString
+
+    }, []);
+    const timeToSeconds = useCallback((timeArray: string[]): number => {
+        const [hours, minutes, seconds] = timeArray.map((t) => parseInt(t.trim(), 10));
+        const totalSeconds = (hours * 60 * 60) + (minutes * 60) + seconds;
+        return totalSeconds;
+    }, []);
+
+    const TimerDisplay = ({ hr, mn, sc }: any) => {
+        if (hr < 10) {hr = '0' + hr;}
+        if (mn < 10) {mn = '0' + mn;}
+        if (sc < 10) {sc = '0' + sc;}
+        return (
+            <div className='timer-display'>
+                <div className='timer-display-num'>
+                    {(hr === 0 || hr === '00') ?
+                        ((mn === 0 || mn === '00') ?
+                            (<span> {sc} </span>)
+                            : (<span> {mn} : {sc} </span>))
+                        : (<span> {hr} : {mn} : {sc}</span>)}
+                </div>
+                <div className='custom-tab-btn timer-display-btn' onClick={() => { setTimerGoing(!timerGoing) }}>
+                    {timerGoing ? <FaPause /> : <FaPlay />}
+
+
+                </div>
+            </div>
+        )
+    }
+
+    const TimerCountdown = ({ remainingTime }: any) => {
+        const hours = Math.floor(remainingTime / 3600)
+        const minutes = Math.floor((remainingTime % 3600) / 60)
+        const seconds = remainingTime % 60
+        // console.log(remainingTime)
+        // console.log(hours, minutes, seconds)
+        if (remainingTime === 0) {
+            return (<div> <TimeInput onSetTime={startTimer} /> </div>)
+        } else {
+            return (
+                <TimerDisplay hr={hours} mn={minutes} sc={seconds} />
+            )
+        }
+
+    }
+
     return (
         <div className='recipe-utility-timer-container'>
             <h5 className='me-auto'>Timer:</h5>
-            <div className='recipe-utility-timer-header'>
-                <input className='timer-hour' type='number' max={99} placeholder='hour' onChange={(e) => { changeTimerTime(parseInt(e.target.value)) }} />
-                <input className='timer-min' type='number' max={59} defaultValue={timerTime} onChange={(e) => { changeTimerTime(parseInt(e.target.value)) }} />
-                <input className='timer-sec' type='number' max={59} defaultValue={timerTime} onChange={(e) => { changeTimerTime(parseInt(e.target.value)) }} />
-                <TimeInput onSetTime={setTimerTime} />
-            </div>
+            {/* <TimeInput onSetTime={startTimer} /> */}
             <div className='recipe-utility-timer-body'>
                 <div >
                     <CountdownCircleTimer
                         key={key}
                         isPlaying={timerGoing}
-                        duration={timerTime}
+                        duration={timerTimeSeconds}
                         colors={['#004777', '#F7B801', '#A30000', '#A30000']}
                         colorsTime={[7, 5, 2, 0]}
+                        children={TimerCountdown}
+                        size={200}
+                        strokeLinecap='butt'
                         onComplete={() => {
                             setTimerGoing(false)
-                            return { delay: 0, newInitialRemainingTime: timerTime }
+                            return { delay: 0, newInitialRemainingTime: timerTimeSeconds }
                         }}
-                    >
-                        {({ remainingTime }) => remainingTime}
-                    </CountdownCircleTimer>
+                    />
+                    {/* <TimerCountdown remainingTime={remainingTime} />
+                        <div>Hey YA</div> */}
+                    {/* </CountdownCircleTimer> */}
                 </div>
-                <div className='custom-tab-btn' onClick={() => { setTimerGoing(!timerGoing) }}>
-                    {timerGoing ? 'Pause' : 'Start'}
-                </div>
-            </div>
+                {/* <div className='custom-tab-btn' onClick={() => { setTimerGoing(!timerGoing) }}>
+                    {timerGoing ? <FaPause /> : <FaPlay />}
 
+
+                </div> */}
+                {(!timerGoing && elapsedTime !== 0)?
+                    <div className='custom-tab-btn reset-btn' onClick={() => { resetTimer() }}>
+                        Reset
+                    </div> : null}
+            </div>
+            {/* 
             <div className='custom-tab-btn' onClick={() => { changeTimerTime(2) }}>
                 reset
-            </div>
+            </div> */}
         </div>
     )
 }
